d3ce63de99f389867145efdc00760fd1d24e51d1
harsh-05 <harshverma09022001@gmail.com>
2025-09-30 16:03:59 +0530

    Initialized db schema and other necessary installations

---
+++ Backend/src/index.ts
@@ -0,0 +1,18 @@
+
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+
+
+const app = express();
+
+const prisma = new PrismaClient();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", async (req, res) => {
+    
+})
+
+app.listen(port, () => {
+    console.log(`App is listening in port ${port}`)
+})
